383282de6c69f5a16960387127136ac18d5b4209
diacoda <ddia@hotmail.com>
2018-04-13 20:57:59 -0400

    added model

---
+++ src/CarFunction/model/Car.cs
@@ -0,0 +1,8 @@
+namespace CarFunction.Model {
+    public class Car {
+        public string Model {get; set;}
+        public string Make {get; set;}
+        public int Year {get; set;}
+        public int Mileage {get; set;}
+    }
+}
